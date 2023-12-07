275b58c49ce517846efc791733149403a47307e1
FrigoMatteo <matteo.frigo-4@studenti.unitn.it>
2023-12-07 15:25:12 +0100

    Implementation of cases example for the tool GPS

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -1,9 +1,37 @@
 use crate::{
-    robotics_lib::interface::{craft, discover_tiles, look_at_sky, teleport, where_am_i},
     robotics_lib::world::tile::{Tile,TileType,Content},
-    robotics_lib::world::*,
+    robotics_lib::world::tile::TileType::*,
+    robotics_lib::world::tile::Content::*,
+    robotics_lib::world::environmental_conditions::EnvironmentalConditions,
+    robotics_lib::world::environmental_conditions::WeatherType::*,
+    robotics_lib::runner::Robot,
+    robotics_lib::runner::Runnable,
+    robotics_lib::runner::backpack::BackPack,
+    robotics_lib::runner::Runner,
+    robotics_lib::event::events::Event,
+    robotics_lib::energy::Energy,
+    robotics_lib::world::coordinates::Coordinate,
+    robotics_lib::world::worldgenerator::Generator,
+    robotics_lib::world::World,
+    robotics_lib::utils::{go_allowed, LibError, calculate_cost_go_with_environment},
+    robotics_lib::interface::{Direction,Tools,where_am_i,craft, debug, destroy, go, look_at_sky, teleport, Direction::*},
 };
 
-pub fn tools(){
-
-}
+/*pub fn attuatore (comandi: &[Direction], mut robot: &Runner, mut world: &World) -> Result<(), LibError>{
+    for c in comandi.iter() {
+        while match go(&mut robot, &mut world, *c.clone()) {
+            Ok(_) => true,
+            Err(error) => {
+                match error {
+                    LibError::NotEnoughEnergy => {
+                        //faccio passare un tick
+                        world.advance_time();
+                    }
+                    _ => Err(error)
+                }
+                false
+            }
+        }{}
+    }
+    Ok(())
+}*/
